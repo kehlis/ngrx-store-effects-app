eeed93e091ea1d1f0424ffa43c1143aa006e733e
Keith Ehlis <keith@ehlis.com>
2018-02-21 14:33:59 -0600

    Finished Routing via Dispatch section.

--- src/products/store/effects/pizzas.effect.ts
+++ src/products/store/effects/pizzas.effect.ts
@@ -4,6 +4,7 @@ import { Effect, Actions } from '@ngrx/effects';
 import { of } from 'rxjs/observable/of';
 import { map, switchMap, catchError } from 'rxjs/operators';
 
+import * as fromRoot from '../../../app/store';
 import * as pizzaActions from '../actions/pizzas.action';
 import * as fromServices from '../../services';
 
@@ -40,6 +41,13 @@ export class PizzasEffects {
         );
 
     @Effect()
+    createPizzaSuccss$ = this.actions$.ofType(pizzaActions.CREATE_PIZZA_SUCCESS)
+        .pipe(
+            map((action: pizzaActions.CreatePizzaSuccess) => action.payload),
+            map(pizza => new fromRoot.Go({path: ['/products', pizza.id]}))
+        );
+
+    @Effect()
     updatePizza$ = this.actions$.ofType(pizzaActions.UPDATE_PIZZA)
         .pipe(
             map((action: pizzaActions.UpdatePizza) => action.payload),
@@ -62,6 +70,16 @@ export class PizzasEffects {
                         map(() => new pizzaActions.RemovePizzaSuccess(pizza)),
                         catchError(error => of (new pizzaActions.RemovePizzaFail(error)))
                     );
+            })
+        );
+
+    @Effect()
+    handlePizzaSuccess$ = this.actions$.ofType(pizzaActions.UPDATE_PIZZA_SUCCESS, pizzaActions.REMOVE_PIZZA_SUCCESS)
+        .pipe(
+            map(pizza => {
+                return new fromRoot.Go({
+                    path: ['/products']
+                })
             })
         );
 }
